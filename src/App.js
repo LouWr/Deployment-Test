90ba0478c99743047e458c479bf934b3e664c8f7
IWLW <87724617+InstantW@users.noreply.github.com>
2022-12-05 15:56:38 +0000

    added h3

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <header className="App-header">
         <h1>Hello World</h1>
         <h2>duiabnwduiabnwuidduib</h2>
+        <h3>This is a h3</h3>
       </header>
     </div>
   );
